9c87f8127dec2480485fab2911e6082b999e0398
flaamdeen <106433522+flaamdeen@users.noreply.github.com>
2023-04-25 19:48:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://putsecrets.onrender.com/auth/google/secrets",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
